f616abed300d5b97b41b7b3ffe179aef9ba5e542
Chunpeng Huo <huocp@me.com>
2022-04-09 20:52:28 +1000

    fix: no need ts plugin. Use pre before vite core plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig, normalizePath } from 'vite';
 import { preprocess } from '@aurelia/plugin-conventions';
-import typescript from 'rollup-plugin-typescript2';
+// import typescript from 'rollup-plugin-typescript2';
 import path from 'path';
 
 export default defineConfig({
-	plugins: [ au2(), typescript() ]
+	plugins: [ au2() ]
 });
 
 function au2() {
 	return {
 		name: 'au2',
+		enforce: 'pre',
 		transform(src, id) {
 			if (id.includes('node_modules')) return;
 			if (id.endsWith('.html') || id.endsWith('.ts')) {
