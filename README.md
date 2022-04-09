8fa69c0f2e76055a17f79102dfe5fb02c5ad8604
Chunpeng Huo <huocp@me.com>
2022-04-09 11:24:48 +1000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aurelia 2 Vite demo
+
+Inside vite.config.ts, there is rollup plugin wrapping @aurelia/plugin-conventions.
+
+    npm run dev
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, normalizePath } from 'vite';
+import { preprocess } from '@aurelia/plugin-conventions';
+import typescript from 'rollup-plugin-typescript2';
+import path from 'path';
+
+export default defineConfig({
+	plugins: [ au2(), typescript() ]
+});
+
+function au2() {
+	return {
+		name: 'au2',
+		transform(src, id) {
+			if (id.includes('node_modules')) return;
+			if (id.endsWith('.html') || id.endsWith('.ts')) {
+				const result = preprocess({
+					path: normalizePath(path.relative(process.cwd(), id)),
+					contents: src
+				}, {})
+				return result;
+			}
+		}
+	}
+}
