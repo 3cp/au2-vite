4515728518dc5b90c1823ff7e877fd056959ba0c
Chunpeng Huo <huocp@me.com>
2022-04-09 22:09:39 +1000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,35 @@
 import { defineConfig, PluginOption } from 'vite';
 import { preprocess } from '@aurelia/plugin-conventions';
+import { createFilter } from '@rollup/pluginutils';
 
 export default defineConfig({
-	plugins: [ au2() ]
+	plugins: [ au2({ include: 'src/**/*.{ts,html}' }) ]
 });
 
-function au2() {
+function au2(options = {}) {
+	const filter = createFilter(options.include, options.exclude);
+
 	return {
 		name: 'au2',
 		enforce: 'pre',
 		transform(code: string, id: string) {
-			if (id.includes('node_modules')) return;
-			if (id.endsWith('.html') || id.endsWith('.ts')) {
-				const result = preprocess({
-					path: id,
-					contents: code
-				}, {})
-				return result;
-				// const part = {
-				// 	code: result.code,
-				// 	// https://rollupjs.org/guide/en/#source-code-transformations
-				// 	// Rollup doc says rollup only needs mappings,
-				// 	// but this does not work inside Vite.
-				// 	// Might be Vite did not expect there is incoming sourcemap
-				// 	// before Vite's core plugins.
-				// 	map: { mappings: result.map.mappings }
-				// };
-				// console.log(part);
-				// return part;
-			}
+			if (!filter(id)) return;
+			const result = preprocess({
+				path: id,
+				contents: code
+			}, {})
+			return result;
+			// const part = {
+			// 	code: result.code,
+			// 	// https://rollupjs.org/guide/en/#source-code-transformations
+			// 	// Rollup doc says rollup only needs mappings,
+			// 	// but this does not work inside Vite.
+			// 	// Might be Vite did not expect there is incoming sourcemap
+			// 	// before Vite's core plugins.
+			// 	map: { mappings: result.map.mappings }
+			// };
+			// console.log(part);
+			// return part;
 		}
 	} as PluginOption;
 }
